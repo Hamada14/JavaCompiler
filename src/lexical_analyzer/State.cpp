1056097a535d4c3951849f2ab347e368d46df903
Moustafa Mahmoud <moustafamahmoud769@gmail.com>
2018-03-21 19:54:29 +0200

    DFA Builder --> added missing epsillon closure

--- src/lexical_analyzer/State.cpp
+++ src/lexical_analyzer/State.cpp
@@ -1,45 +1,56 @@
 #include "lexical_analyzer/State.hpp"
 
-set<int>* State::get_nodes() {
+set<int>* State::get_nodes()
+{
     return nodes;
 }
 
-int State::get_id() {
+int State::get_id()
+{
     return id;
 }
 
-int State::get_priority() {
+int State::get_priority()
+{
     return priority;
 }
 
-string State::get_type() {
+string State::get_type()
+{
     return type;
 }
 
-bool State::get_acceptance() {
+bool State::get_acceptance()
+{
     return is_accepteance;
 }
 
-void State::set_nodes(set<int> *n) {
+void State::set_nodes(set<int>* n)
+{
     nodes = n;
 }
 
-void State::insert_node(int n) {
+void State::insert_node(int n)
+{
     nodes->insert(n);
 }
 
-void State::set_id(int idx) {
+void State::set_id(int idx)
+{
     id = idx;
 }
 
-void State::set_priority(int prio) {
+void State::set_priority(int prio)
+{
     priority = prio;
 }
 
-void State::set_type(string s) {
+void State::set_type(string s)
+{
     type = s;
 }
 
-void State::set_acceptance(bool s) {
+void State::set_acceptance(bool s)
+{
     is_accepteance = s;
 }
