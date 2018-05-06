a75fd7cdfd22b83adfa52ad26a53644c3d2d7981
Ahmed Abdellah <ahmedabdela00000000@gmail.com>
2018-05-06 07:15:57 +0300

    fixing error on mac  ^_^

--- src/parser/PredictiveTable.cpp
+++ src/parser/PredictiveTable.cpp
@@ -1,7 +1,5 @@
 #include "parser/PredictiveTable.hpp"
 
-#include "Config.hpp"
-
 #include <algorithm>
 #include <cassert>
 #include <iostream>
@@ -10,16 +8,10 @@
 PredictiveTable:: PredictiveTable( std::map<std::string, std::vector<ProductionRule> > ll1_grammar, std::string start_state)
 : ll1_grammar (ll1_grammar), start_state(start_state) {
     for(auto& it: ll1_grammar) {
-        getFirst(it.first);
         getFollow(it.first);
+        getFirst(it.first);
     }
-    setPossibleTransitions();
-    print_predictive_table = new PrintPredictiveTable(Config::getInstance()->get(Config::PREDICTIVE_TABLE_PATH_KEY));
-    std::vector<string> head(1, "Non Terminal");
-    for (auto& itr : possible_transitions)
-        head.push_back(itr);
-    print_predictive_table->printHeader(head);
-    printTable();
+
 }
 
 std::string PredictiveTable::getStartState() {
@@ -28,6 +20,7 @@ std::string PredictiveTable::getStartState() {
 
 bool PredictiveTable:: checkTerminals(RuleToken &r, std::unordered_set<std::string> &cur, std::string &state, ProductionRule &pr, TYPE type) {
     cur.insert(r.getValue());
+
     if(type == TYPE::FIRST)
         table[state][r.getValue()] = pr;
     if(r.getType() == RuleTokenType::TERMINAL)
@@ -36,7 +29,7 @@ bool PredictiveTable:: checkTerminals(RuleToken &r, std::unordered_set<std::stri
     return true;
 }
 
-void PredictiveTable::addFollowToTable(std::string state) {
+void PredictiveTable:: addFollowToTable(std::string state) {
     for(ProductionRule &pr: ll1_grammar[state]) {
         std::vector<RuleToken> tokens = pr.getTokens();
         bool has_lambda = true;
@@ -83,10 +76,11 @@ std::unordered_set<std::string> PredictiveTable:: getFirst(std::string state) {
         }
         if(has_lambda) cur_first.insert(Constants::LAMBDA);
     }
-
+    
     addFollowToTable(state);
     return cur_first;
 }
+
 
 std::unordered_set<std::string> PredictiveTable:: getFollow(std:: string state) {
     if(!ll1_grammar.count(state))
@@ -104,7 +98,8 @@ std::unordered_set<std::string> PredictiveTable:: getFollow(std:: string state) 
             for(int i = 0; i < (int)tokens.size() - 1 ; ++i) {
                 if(tokens[i].getValue() == state) {
                     std::unordered_set<std::string> cur_first;
-                    if(tokens[i+1].getType() == RuleTokenType::NON_TERMINAL) cur_first = getFirst(tokens[i+1].getValue());
+                    if(tokens[i+1].getType() == RuleTokenType::NON_TERMINAL) 
+                    	cur_first = getFirst(tokens[i+1].getValue());
                     else cur_first.insert(tokens[i+1].getValue());
                     std::unordered_set<std::string>:: iterator it = cur_first.find(Constants:: LAMBDA);
                     if(it != cur_first.end()) cur_first.erase(it);
@@ -116,7 +111,8 @@ std::unordered_set<std::string> PredictiveTable:: getFollow(std:: string state) 
                     std::unordered_set<std::string> new_follow = getFollow(prs.first);
                     cur_follow.insert(new_follow.begin(), new_follow.end());
                 }
-                if(tokens[i].getType() == RuleTokenType::NON_TERMINAL && !getFirst(tokens[i].getValue()).count(Constants::LAMBDA))
+                if(tokens[i].getType() == RuleTokenType::NON_TERMINAL && 
+                	!getFirst(tokens[i].getValue()).count(Constants::LAMBDA))
                     break;
                 if(tokens[i].getType() == RuleTokenType::TERMINAL)
                     break;
@@ -140,34 +136,4 @@ TransitionType PredictiveTable::getTransitionType(std::string state, std::string
     if(getFollow(state).count(input))
         return TransitionType::SYNC;
     return TransitionType::ERROR;
-}
-
-void PredictiveTable::setPossibleTransitions() {
-    for (auto& itr : ll1_grammar) {
-        for (auto& pr : itr.second) {
-            for (auto& tk : pr.getTokens()) {
-                if (tk.getType() == RuleTokenType::TERMINAL)
-                    possible_transitions.insert(tk.getValue());
-            }
-        }
-    }
-}
-
-void PredictiveTable::printTable() {
-    for (auto& itr : ll1_grammar) {
-        std::vector<string> row(1, itr.first);
-        for (auto& input : possible_transitions) {
-            if (getTransitionType(itr.first, input) == TransitionType::ERROR)
-                row.push_back("ERROR");
-            if (getTransitionType(itr.first, input) == TransitionType::SYNC)
-                row.push_back("SYNC");
-            if (getTransitionType(itr.first, input) == TransitionType::LEGAL) {
-                std::string nxt = "";
-                for (auto& trans : getTransition(itr.first, input))
-                    nxt += trans.getValue() + " ";
-                row.push_back(nxt);
-            }
-        }
-        print_predictive_table->printData(row);
-    }
 }
